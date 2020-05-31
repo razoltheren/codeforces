1a4a8ea811e4f693d6345861bdd1b9b661fd426e
razoltheren <ishitabatra0809@gmail.com>
2020-05-31 15:18:27 +0530

    In order

---
+++ 4-A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	unsigned int n;
+	cin>>n;
+	if(n>100){
+		return 0;
+	}
+	if(n%2==0&&n>2){
+		cout<<"YES";
+
+	}
+	else{
+		cout<<"NO";
+	}
+	return 0;
+}
